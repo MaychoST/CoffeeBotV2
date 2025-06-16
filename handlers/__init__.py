f9ba9a1effc94a0eb6d7e0981a1c1a8ead057111
MaychoST <s_2may1_t@mail.ru>
2025-06-16 19:20:05 +0600

    refactor: Implement robust lifespan management

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,16 +1,16 @@
-# Имя файла: handlers/__init__.py (ВРЕМЕННАЯ ОТЛАДОЧНАЯ ВЕРСИЯ 3)
+# Имя файла: handlers/__init__.py (ВРЕМЕННАЯ ОТЛАДОЧНАЯ ВЕРСИЯ 4)
 
 from .start_handler import router as start_router
 from .common_handler import router as common_router
 from .order_handler import router as order_router
-from .staff_handler import router as staff_router  # <-- РАСКОММЕНТИРОВАТЬ
-
-# from .admin_menu_management_handler import router as admin_menu_management_router
+from .staff_handler import router as staff_router
+from .admin_menu_management_handler import router as admin_menu_management_router # <-- РАСКОММЕНТИРОВАТЬ
 # from .report_handler import router as report_router
 
 __all__ = [
     "start_router",
-    "common_router",
+    "common_handler",
     "order_router",
     "staff_router",
+    "admin_menu_management_router", # <-- РАСКОММЕНТИРОВАТЬ
 ]
